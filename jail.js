6b8da95a87bb9ecc5a78e230d79e517cb81094ee
silenti88 <45247402+silenti88@users.noreply.github.com>
2019-01-11 22:01:17 -0500

    Updated (1/11 10:00PM EST)

--- jail.js
+++ jail.js
@@ -5,12 +5,12 @@ module.exports.run = async (bot, message, args) => {
     let Jailor = message.guild.roles.get("532752296593981452");
     if(message.member.roles.some(r=>["new role", "Host"].includes(r.name))){
         let tagged = message.mentions.users.first();
-        let jailed = tagged.id;
+        let jailed = tagged.username;
         message.channel.overwritePermissions(jailed, {
             SEND_MESSAGES: true,
             READ_MESSAGE_HISTORY: false
         })
-        message.channel.send(`${message.author} Please remember to unjail them at day`);  
+        message.channel.send(`${message.author} You have successfully jailed ${jailed}`);  
     }
 }
 
