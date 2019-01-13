c5ebcd939c5ea9b15823c1a02c3945f6e4c6ba24
Gangster8192 <30675315+Gangster8192@users.noreply.github.com>
2019-01-13 18:04:12 -0500

    Changed the permissions, removed the testing roles permission, getting ready for match.

--- Commands/reveal.js
+++ Commands/reveal.js
@@ -2,9 +2,9 @@ const Discord = require("discord.js");
 
 
 module.exports.run = async (bot, message, args) => {
-    let testing = '533782866694504459';
+    let placeholder = '533782866694504459';
     let Mayor = '533794447607988250';
-    if(message.member.roles.has(testing)){
+    if(message.member.roles.has(placeholder)){
         message.channel.send(`${message.author} has revealed themselves as Mayor`);
         message.member.addRole(Mayor).catch(console.error);
     }else{
