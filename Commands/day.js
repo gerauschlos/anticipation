e86ec628c48da06c27db87b9ae259c389f159f24
Dominic Humber <dhumber2003@gmail.com>
2019-01-13 20:22:03 -0500

    Updates
    Changed jail and day commands,
    mostly Jail

--- Commands/day.js
+++ Commands/day.js
@@ -3,8 +3,9 @@ var previousJailed = require('./jail.js');
 
 
 module.exports.run = async (bot, message, args) => {
+    console.log(previousJailed);
     let match_members = message.guild.roles.get("529053936120758303"); //UPDATED
-    if(message.member.roles.some(r=>["God", "Host", "testing"].includes(r.name))){
+    if(message.member.roles.some(r=>["Admin", "Host", "Testers"].includes(r.name))){
         message.channel.overwritePermissions(match_members, {
             VIEW_CHANNEL: true,
             SEND_MESSAGES: true
--- Commands/jail.js
+++ Commands/jail.js
@@ -2,31 +2,26 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
+    
     let general_chat = "mainmatch";
     let deadchat = "graveyard"
     let location = message.channel.name;
-
+   
     
     if(location === general_chat || location === deadchat) return;
     console.log("YES");
 
-    if(message.member.roles.some(r=>["testing", "Host", "God"].includes(r.name))){
+    if(message.member.roles.some(r=>["Testers", "Host", "Admin"].includes(r.name))){
         let tagged = message.mentions.roles.first();
         let jailed = tagged.id;
- //       console.log(jailed);
-        let cell = "523881129149726721"; //UPDATED
-         //     exports.person = jailed;
-        bot.channels.get(cell).overwritePermissions(jailed, {
-            SEND_MESSAGES: true,
-            READ_MESSAGE_HISTORY: false,
-            READ_MESSAGES: true,
-        })
-          .then(updated => console.log(updated.permissionOverwrites.get(jailed)))
-          .catch(console.error)   
+
+        let cell = tagged.name; //UPDATED   
         jailed = tagged;
-        bot.channels.get(cell).send(`${jailed} you have been jailed`);
-        bot.channels.get(cell).send("Use '>send [message]' to talk to the jailor");
-        message.channel.send(`${message.author} You have successfully jailed ${jailed}`);  
+        
+        message.guild.channels.find(channel => channel.name === cell).send(`${jailed} you have been jailed`);
+        message.guild.channels.find(channel => channel.name === cell).send("Use '!?send [message]' to talk to the jailor");
+        message.channel.send(`${message.author} You have successfully jailed the user`); 
+        exports.person = jailed; return 
     }
 }
 
