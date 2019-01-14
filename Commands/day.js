d3c8bc62cd19c0fbc955560f72b8b5208f3aaac8
Dominic Humber <dhumber2003@gmail.com>
2019-01-13 22:51:23 -0500

    Updates
    Still working on it. (send/jail command)

--- Commands/day.js
+++ Commands/day.js
@@ -3,27 +3,27 @@ var previousJailed = require('./jail.js');
 
 
 module.exports.run = async (bot, message, args) => {
-    console.log(previousJailed);
+    
     let match_members = message.guild.roles.get("529053936120758303"); //UPDATED
+
     if(message.member.roles.some(r=>["Admin", "Host", "Owner"].includes(r.name))){
         message.channel.overwritePermissions(match_members, {
             VIEW_CHANNEL: true,
             SEND_MESSAGES: true
         })
           .then(updated => console.log(updated.permissionOverwrites.get(match_members)))
-          .catch(console.error)
-    let cell = "523881129149726721"; //UPDATED
-    bot.channels.get(cell).overwritePermissions(previousJailed, {
-            VIEW_CHANNEL: false,
-            SEND_MESSAGES: false
-        })
-          .then(updated => console.log(updated.permissionOverwrites.get(previousJailed)))
-          .catch(console.error)      
+          .catch(console.error)   
         
         message.channel.send("🌄 Sunrise")
+        
+        for(let i=0; i<26; i++ ){
+            message.guild.channels.find(channel => channel.name === i).send(`---------------------------------**DAY TIME**----------------------------------`);
+        }
+
     }else{
         message.channel.send(`You are not the host of this game!`);
     }
+    
 }
 
 module.exports.help = {
--- Commands/jail.js
+++ Commands/jail.js
@@ -6,22 +6,46 @@ module.exports.run = async (bot, message, args) => {
     let general_chat = "mainmatch";
     let deadchat = "graveyard"
     let location = message.channel.name;
-   
+    let message_sent = ''; 
+    let message_channel = message.channel.id;
+    let jailor_channel = "533791950080573440";//UPDATED
+    let tagged = message.mentions.roles.first();
+    let jailed = tagged;
+    let cell = tagged.name; //UPDATED   
     
     if(location === general_chat || location === deadchat) return;
-    console.log("YES");
+
+    
+    for(let i=3; i<args.length; i++ ){
+         message_sent += args[i];
+         console.log(message_sent);
+    }
+
+    if(message.member.roles.has(r=>[cell,"Host", "Admin"])){
+    if(args[0] === "send"){
+        if(cell === message_channel){
+
+            bot.channels.get(jailor_channel).send(`${message.author}: `+ message_sent);
+            message.channel.send("sent");
+            return;
+        }else{
+
+            if(jailor_channel === message_channel){
+            bot.channels.get(cell).send("**Jailor:** "+ message_sent);
+            message.channel.send("sent");
+            return;
+            }
+        }
+    }
+    }
 
     if(message.member.roles.some(r=>["Host", "Admin", "Owner"].includes(r.name))){
-        let tagged = message.mentions.roles.first();
-        let jailed = tagged.id;
-
-        let cell = tagged.name; //UPDATED   
-        jailed = tagged;
         
         message.guild.channels.find(channel => channel.name === cell).send(`${jailed} you have been jailed`);
         message.guild.channels.find(channel => channel.name === cell).send("Use '!?send [message]' to talk to the jailor");
         message.channel.send(`${message.author} You have successfully jailed the user`);  
     }
+
 }
 
 module.exports.help = {
