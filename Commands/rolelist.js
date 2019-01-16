b230ed6a00bffa5b937f695ceb0e0fc54a81277e
Gangster8192 <30675315+Gangster8192@users.noreply.github.com>
2019-01-15 20:55:26 -0500

    Added !?rolelist command
    DO NOT USE THIS IN A CHANNEL THAT THE PUBLIC CAN SEE
    
    YOU HAVE BEEN WARNED

---
+++ Commands/rolelist.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const fs = require ("fs");
+
+module.exports.run = async (bot, message, args) => {
+
+    var commandList = fs.readFileSync('Commands/commands.txt', 'utf8');
+
+    if(message.member.roles.some(r=>["Owner", "Programmers"].includes(r.name))){
+
+        message.channel.send(commandList);
+    } else {
+
+        message.channel.send(`**Error:** 403 Forbidden`)
+    }
+
+}
+
+module.exports.help = {
+    name: "rolelist"
+}
