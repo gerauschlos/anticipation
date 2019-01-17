d9ce570b8de52b9865de2c1a4958978ade1f1920
Dominic Humber <dhumber2003@gmail.com>
2019-01-16 19:25:15 -0500

    Update night.js

--- Commands/night.js
+++ Commands/night.js
@@ -5,6 +5,7 @@ const important = require("../important.js");
 module.exports.run = async (bot, message, args) => {
     // console.log("Important: "+important.getday());
     let cell = important.getjailed();
+    console.log(important.getjailed());
     let match_members = message.guild.roles.get("529053936120758303"); //UPDATED
     if(message.channel.name ==! "mainmatch") return;
     if(message.member.roles.some(r=>["Admin", "Host", "Owner"].includes(r.name))){
@@ -16,9 +17,9 @@ module.exports.run = async (bot, message, args) => {
           .catch(console.error)
         message.channel.send("🌃 Sunset");
         important.setday(false);
-        if(cell !== " "){
-            message.guild.channels.find(channel => channel.name === important.getjailed()).send(`${cell.tag} you have been jailed`);
-            message.guild.channels.find(channel => channel.name === important.getjailed()).send("Use '!?send [message]' to talk to the jailor");
+        if(cell ==! " "){
+            message.guild.channels.find(channel => channel.name === cell).send(`You have been jailed`);
+            message.guild.channels.find(channel => channel.name === cell).send("Use '!?send [message]' to talk to the jailor");
             message.guild.channels.find(channel => channel.name === "jail").send(`You have successfully jailed the user`); 
         }else{
             message.guild.channels.find(channel => channel.name === "jail").send(`You forgot to jail someone :weary::ok_hand: `);
