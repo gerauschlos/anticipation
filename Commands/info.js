55d27a224388efe3424c804cc4e739c9fd0a8196
Dominic Humber <dhumber2003@gmail.com>
2019-01-19 21:28:02 -0500

    Fixed

--- Commands/info.js
+++ Commands/info.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
     let args_ = lowerCase(args[1]);
 
 
-    if(args_!=="trickster" || args_!=="houndmaster" || args_!=="cleric" || args_!=="ninja" || args_!== "officer" || args_!=="shapeshifter" ) {
+    if(args_!==" trickster" && args_!==" houndmaster" && args_!==" cleric" && args_!==" ninja" && args_!== " officer" && args_!==" shapeshifter" ) {
         message.channel.send(`Visit http://town-of-salem.wikia.com/wiki/Roles for the original roles.`);
     } else {
         if(args_===" trickster"){
