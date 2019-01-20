f139e6351fa541e40cafcb6359c353e2e71dfc29
Dominic Humber <dhumber2003@gmail.com>
2019-01-20 14:11:15 -0500

    Update action.js

--- commands/action.js
+++ commands/action.js
@@ -12,9 +12,9 @@ module.exports.run = async (bot, message, args) => {
       //console.log(args[1]);
       //if(args[1] !== " kill" || args[1] !== " protect" || args[1] !== " invest") return;
 
-        for(let i=0; i<args.length; i++ ){
+    /*    for(let i=0; i<args.length; i++ ){
             message_sent += args[i];
-        }  
+        }  */
         bot.channels.get(logs).send(player+"-"+message_sent)  ;
 }
 
