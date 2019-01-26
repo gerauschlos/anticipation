07f87b2d3b1523426f88d87876a185f0cf2fdeda
Dominic Humber <dhumber2003@gmail.com>
2019-01-26 03:10:12 -0500

    Adjustments
    Status: Not Finished {I know what I am going to do tho}

--- commands/action.js
+++ commands/action.js
@@ -7,6 +7,29 @@ module.exports.run = async (bot, message, args) => {
         let general_chat = "523693033825501190"; //UPDATED
         let location = message.channel.parent.name;
         let player = `${message.author.username}`;
+        var role = new Map();
+
+        role.set("1","Bodygaurd,Godfather,Arsonist or Crusader")
+        role.set("2", "Lookout, Forger, Coven Leader")
+        role.set("3", "")
+        role.set("4")
+        role.set("5")
+        role.set("6")
+        role.set("7")
+        role.set("8")
+        role.set("9")
+        role.set("10")
+        role.set("11")
+        role.set("12")
+        role.set("13")
+        role.set("14")
+        role.set("15")
+        role.set("16")
+        role.set("17")
+        role.set("18")
+        role.set("19")
+        role.set("20")
+
 
         if(location !== "Player Chats") return;
       //console.log(args[1]);
