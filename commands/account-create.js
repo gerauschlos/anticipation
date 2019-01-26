9c031bd82bb2bfbd31aba22345ec17ebe9c7e1de
Gangster8192 <30675315+Gangster8192@users.noreply.github.com>
2019-01-25 23:36:07 -0500

    Economy

--- commands/account-create.js
+++ commands/account-create.js
@@ -5,14 +5,21 @@ module.exports.run = async (bot, message, args) => {
 
     let sender = message.author
     let userData = JSON.parse(fs.readFileSync('Storage/userData.json', 'utf-8'));
-    
-    if (!userData[sender.id + message.guild.id]) userData[sender.id + message.guild.id] = {}
-    if (!userData[sender.id + message.guild.id].money) userData[sender.id + message.guild.id].money = 0;
-    
-    fs.writeFile('Storage/userData.json', JSON.stringify(userData), (err) => {
-        if (err) console.error(err)
 
-})
+    if (!userData[sender.id + message.guild.id]) {
+        userData[sender.id + message.guild.id] = {}
+        userData[sender.id + message.guild.id].money = 0;
+        message.channel.send(`✅ Account Created! Use !balance to check your balance.`)
+        
+        fs.writeFile('Storage/userData.json', JSON.stringify(userData), (err) => {
+            if (err) console.error(err)
+    })
+
+    if(userData[sender.id + message.guild.id]) {
+        message.channel.send(`You already have a account! <:ree:538562884909662223>`)
+        return
+    }
+}
 
 }
 
--- commands/balance.js
+++ commands/balance.js
@@ -7,20 +7,28 @@ module.exports.run = async (bot, message, args) => {
     let userData = JSON.parse(fs.readFileSync('./Storage/userData.json', 'utf-8'));
     let sender = message.author
 
-    message.channel.send({embed:{
-        title: "Bank",
-        color: 0xF1C40F, 
-        fields:[{
-            name:"Account Holder",
-            value:message.author.username,
-            inline:true
-        },
-        {
-            name:"Account Balance",
-            value:userData[sender.id + message.guild.id].money,
-            inline:true
-        }]
-    }})
+    if(userData[sender.id + message.guild.id]) {
+
+        message.channel.send({embed:{
+            title: "Bank",
+            color: 0xF1C40F, 
+            fields:[{
+                name:"Account Holder",
+                value:message.author.username,
+                inline:true
+            },
+            {
+                name:"Account Balance",
+                value:userData[sender.id + message.guild.id].money,
+                inline:true
+            }]
+        }})
+    
+    } else {
+        message.channel.send(`⛔ Create an account first!`)
+    }
+
+    
 
 }
 
