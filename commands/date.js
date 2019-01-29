4eb41361a60f0da4a80490812281d1f5581fcc6b
Gangster8192 <30675315+Gangster8192@users.noreply.github.com>
2019-01-28 20:49:18 -0500

    Deletes Message after input

--- commands/date.js
+++ commands/date.js
@@ -5,6 +5,7 @@ const countdown = require('countdown')
 module.exports.run = async (bot, message, args) => {
 
     let matchtime = countdown(new Date(), new Date(2019, 0, 29, 17, 00, 00, 00) ).toString();
+    message.delete().catch();
     message.channel.send(`The match will start in ${matchtime}!`)
 
 }
