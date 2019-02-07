250272053dcc282ee7230f6d8b4d2a1615efac7e
Gangster8192 <30675315+Gangster8192@users.noreply.github.com>
2019-02-06 22:05:18 -0500

    Removed Multipurpose Commands
    Updated Versions of those commands can be found at https://github.com/Gangster8192/Ice-Cold

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,10 @@
 const Discord = require("discord.js");
-const bot = new Discord.Client({disableEveryone: true});
 
 module.exports.run = async (bot, message, args) => {
-//    let location = message.channel.name;
-//    console.log(`Location: ${location}`);
-    message.channel.send(`Pong! | API Latency is ${Math.round(bot.ping)}ms`)
+
+    message.channel.send(`Pong! \nAPI Latency is ${Math.round(bot.ping)}ms`)
 }
+
 module.exports.help = {
     name: "ping"
 }
