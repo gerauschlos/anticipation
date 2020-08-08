9e5091ffde2dc3d87e99e53278763d5f21d7a743
Gangster8192 <30675315+Gangster8192@users.noreply.github.com>
2020-08-08 14:12:12 -0400

    Actually do stuff???/

--- commands/ping.js
+++ commands/ping.js
@@ -1,15 +1,42 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    await message.delete();
-    let pingMessage = await message.channel.send(`Ping is being calculated! :bar_chart:`);
-    let brt = Math.round(pingMessage.createdTimestamp - message.createdTimestamp);
-    let api = Math.round(bot.ping);
-    let yl = Math.round(brt - api);
+  let loadingpingEmbed = new Discord.MessageEmbed()
+    .setTitle("Latency :bar_chart:")
+    .setDescription("Ping is being calculated!")
+    .setColor("#3498DB");
 
-    pingMessage.edit(`:hourglass: Bot Respond Time: ${brt}ms\n:robot: API Latency: ${api}ms\n:label: Your Latency: ${yl}ms`);
-}
+  let loadmessage = await message.channel.send(loadingpingEmbed);
+
+  let totalping = Math.round(
+    loadmessage.createdTimestamp - message.createdTimestamp
+  );
+  let apiping = Math.round(bot.ws.ping);
+  let clientping = "gangster broke something";
+  if (totalping < apiping) {
+    clientping = Math.round(apiping - totalping);
+  } else {
+    clientping = Math.round(totalping - apiping);
+  }
+
+  let loadedpingEmbed = new Discord.MessageEmbed()
+    .setTitle("Latency :bar_chart:")
+    .addField("Bot Response", totalping)
+    .addField("API", apiping)
+    .addField("Client", clientping)
+    .setColor("#3498DB");
+
+  if (totalping < 0 || apiping < 0 || clientping < 0) {
+    let loadedpingEmbed = new Discord.MessageEmbed()
+      .setTitle("Latency 📉")
+      .setDescription("Ping could not be calculated!\nBlame discord's api.")
+      .setColor("#3498DB");
+    loadmessage.edit(loadedpingEmbed);
+  } else {
+    loadmessage.edit(loadedpingEmbed);
+  }
+};
 
 module.exports.help = {
-    name: "ping" // To-Do: Add alias = "Pong";
-}
+  name: "ping",
+};
--- index.js
+++ index.js
@@ -1,79 +1,71 @@
-const discord = require('discord.js');
-const inquirer = require('inquirer');
-const fs = require('fs');
-const config = require('./config.json');
+const discord = require("discord.js");
+const fs = require("fs");
+const config = require("./config.json");
 
 const bot = new discord.Client({
-	// Create discord client, disable the @everyone tag from being abused.
-    disableEveryone: true
+  // Create discord client, disable the @everyone tag from being abused.
+  disableEveryone: true,
 });
 
 bot.commands = new discord.Collection();
 
 // Load the commands from the commands subdirectory.
 fs.readdir("./commands/", (err, files) => {
-    if (err) console.log(err);
+  if (err) console.log(err);
 
-    let jsfile = files.filter(f => f.split(".").pop() === "js");
-	
-    // If no commands are found, ask if to continue, or to exit.
-    if (jsfile.length <= 1) {
-        inquirer.prompt({
-            name: 'startupconfirmation',
-            type: 'confirm',
-            message: 'No commands were found. Continue to start the bot?',
-        }).then(answers => {
-            jsfile.forEach((f, i) => {
+  let cmdfile = files.filter((f) => f.split(".").pop() === "js");
 
-                let props = require(`./commands/${f}`);
-                console.log(`Loading ${f}...`);
-                bot.commands.set(props.help.name, props);
-                exports.help
-        
-            })
-        })
-    }
-	
-	// Load the stuff from each file
+  if (cmdfile.length <= 0) {
+    console.log(`No commands found, loading bot with no commands!`);
+  }
 
-
-})
-
-// Ran on receiving a message
-bot.on('message', async message => {
-    let prefix = config.prefix;
-
-	// If the message is from the bot, doesn't start with the prefix, or
-	// is a DM to the bot, then we can safely ignore it
-    if (!message.content.startsWith(prefix)) return;
-    if (message.author.bot) return;
-    if (message.channel.type === "dm") return;
-
-	// Split the messasge into the command and the arguments
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0];
-    let args = message.content.split(cmd); // not safe, will fuck everything up if one of the args is the command
-	                                       // this looks like it returns a length 1 array with 1 string inside no matter what - is this right?
-    
-	// Run the command. Is there any way to edit variables in this particular scope while in the command scope?
-	// If not, we should look for a different approach - that's gonna be important when it comes to voting
-    if (message.content.startsWith(prefix)) {
-        let commandfile = bot.commands.get(cmd.slice(prefix.length));
-        if (commandfile) commandfile.run(bot, message, args);
-    }
+  cmdfile.forEach((f, i) => {
+    let props = require(`./commands/${f}`);
+    bot.commands.set(props.help.name, props);
+    exports.help;
+  });
 });
 
-bot.on('ready', async () => {
-    console.log(`Anticipation is now ready to receive commands.`);
-	// sets the prefix in the status
-    bot.user.setPresence({
-        game: {
-            name: '| Prefix: ?',
-            type: 'PLAYING',
-        },
-        status: 'online',
-        afk: false,
-    }).catch(err => console.log(err));
+// Ran on receiving a message
+bot.on("message", async (message) => {
+  let prefix = config.prefix;
+
+  // If the message is from the bot, doesn't start with the prefix, or
+  // is a DM to the bot, then we can safely ignore it
+  if (!message.content.startsWith(prefix)) return;
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  // Split the messasge into the command and the arguments
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = message.content.slice(prefix.length + cmd.length).split(" ");
+
+  // Run the command. Is there any way to edit variables in this particular scope while in the command scope?
+  // If not, we should look for a different approach - that's gonna be important when it comes to voting
+  if (message.content.startsWith(prefix)) {
+    let commandfile = bot.commands.get(cmd.slice(prefix.length));
+    if (commandfile) commandfile.run(bot, message, args);
+  }
+});
+
+bot.on("ready", async () => {
+  console.log(`something creative here`);
+  // sets the prefix in the status
+  bot.user
+    .setPresence({
+      game: {
+        name: "myself being developed.",
+        type: "WATCHING",
+      },
+      status: "online",
+      afk: false,
+    })
+    .catch((err) => console.log(err));
+});
+
+bot.on("error", async () => {
+  console.log(error);
 });
 
 bot.login(config.token);
