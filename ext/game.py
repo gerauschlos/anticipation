bb4dafaea3daf6069deb4427e054277a5ac26206
Dominic Humber <dhumber2017@gmail.com>
2020-10-04 13:21:58 -0400

    Redid cog check and changed the exampleconfig
    add stuff

--- ext/game.py
+++ ext/game.py
@@ -1,5 +1,5 @@
 import discord
-from discord import User, Member
+from discord import User, Member, Role, Guild
 from discord.ext.commands import PrivateMessageOnly, BadArgument, Context
 import config
 import re
@@ -15,52 +15,55 @@ class Game(commands.Cog):
 
     def __init__(self, bot: Bot):
         self.bot = bot
-        self.guild = self.bot.get_guild(int(config.guild_id))
-        self.player_role = discord.utils.get(
-            self.guild.roles, id=int(config.role_player_id))
+        self.guild: Guild = self.bot.get_guild(int(config.guild_id))
+        self.player_role: Role = discord.utils.get(
+            self.guild.roles, id=int(config.player_role_id))
+
+    async def cog_check(self, ctx: Context) -> bool:
+        member = self.guild.get_member(ctx.author.id)
+
+        if self.player_role not in member.roles:
+            await ctx.send(f"You need to be in-game to be use the {ctx.command} command")
+            return False
+        return True
 
     @commands.command(name="reveal", aliases=["rev"], enabled=False)
     @commands.guild_only()
     async def reveal(self, ctx: Context):
-
         await ctx.message.delete()
 
-        player_mayor = await commands.UserConverter().convert(ctx, config.player_mayor_id)
-        game_mayor = await commands.RoleConverter().convert(ctx, config.game_mayor)
+        player_mayor = await commands.UserConverter().convert(ctx, str(config.player_mayor_id))
+        mayor_role = await commands.RoleConverter().convert(ctx, str(config.mayor_role_id))
 
         if ctx.author.id is not player_mayor.id:
             return await ctx.author.send("`You are not mayor.`")
 
-        await ctx.author.add_roles(game_mayor, reason="Auto-assign (revealed mayor)")
+        await ctx.author.add_roles(mayor_role, reason="Auto-assign (revealed mayor)")
 
         revealed_message = await ctx.send(f"`{player_mayor} has revealed themselves as mayor!`")
 
         await revealed_message.pin(reason="Auto-pin (revealed mayor)")
 
-    async def can_whisper(self, ctx: Context, reciever: Member, whisperer: Member) -> bool:
+    async def can_whisper(self, ctx: Context, receiver: Member, whisperer: Member) -> bool:
         if config.game_has_mayor is True:
             mayor_role = discord.utils.get(
-                self.guild.roles, id=int(config.role_mayor_id))
+                self.guild.roles, id=int(config.mayor_role_id))
 
             if mayor_role in whisperer.roles:
                 await ctx.author.send("`You cannot whisper as a revealed mayor.`")
                 return False
-            elif mayor_role in reciever.roles:
+            elif mayor_role in receiver.roles:
                 await ctx.author.send("`You cannot whisper to a revealed mayor.`")
                 return False
-
-        if self.player_role not in whisperer.roles:
-            await ctx.send("`You cannot whisper if you are not in a game.`")
-            return False
-        elif self.player_role not in reciever.roles:
-            await ctx.send("`You cannot whisper to someone who is not playing.`")
-            return False
-        elif ctx.author.id == reciever.id:
-            await ctx.send("`You cannot whisper to yourself.`")
-            return False
-
         else:
-            return True
+            if self.player_role not in receiver.roles:
+                await ctx.send("`You cannot whisper to someone who is not playing.`")
+                return False
+            elif ctx.author.id == receiver.id:
+                await ctx.send("`You cannot whisper to yourself.`")
+                return False
+            else:
+                return True
 
     @staticmethod
     async def is_valid_message(ctx: Context, sanitized_message: str) -> bool:
@@ -73,10 +76,9 @@ class Game(commands.Cog):
         else:
             return True
 
-    @staticmethod
-    async def send_whisper(ctx, receiver, whisperer, sanitized_message):
-        mainmatch_channel = await commands.TextChannelConverter().convert(ctx, config.mainmatch_channel_id)
-        whispers_channel = await commands.TextChannelConverter().convert(ctx, config.whispers_channel_id)
+    async def send_whisper(self, ctx: Context, receiver: Member, whisperer: Member, sanitized_message: str):
+        mainmatch_channel = await commands.TextChannelConverter().convert(ctx, str(config.mainmatch_channel_id))
+        whispers_channel = await commands.TextChannelConverter().convert(ctx, str(config.whispers_channel_id))
 
         try:
             await receiver.send(f"`{whisperer.display_name} whispers to you: {sanitized_message}`")
@@ -95,15 +97,15 @@ class Game(commands.Cog):
     @commands.command(name="whisper", aliases=["whisp", "w", "msg"])
     @commands.dm_only()
     async def whipser(self, ctx: Context, user: User, *, message: str):
-        guild = self.bot.get_guild(int(config.guild_id))
+        guild = self.guild
 
-        whisperer = guild.get_member(ctx.author.id)
-        reciever = guild.get_member(user.id)
+        whisperer = await guild.fetch_member(ctx.author.id)
+        receiver = await guild.fetch_member(user.id)
 
         sanitized_message = ext.utils.sanitize_text(message)
 
-        if await self.can_whisper(ctx, reciever, whisperer) and await self.is_valid_message(ctx, sanitized_message):
-            await self.send_whisper(ctx, reciever, whisperer, sanitized_message)
+        if await self.can_whisper(ctx, receiver, whisperer) and await self.is_valid_message(ctx, sanitized_message):
+            await self.send_whisper(ctx, receiver, whisperer, sanitized_message)
 
     @whipser.error
     async def whipser_error(self, ctx: Context, error):
